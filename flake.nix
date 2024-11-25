df133fd6b04b307c8650f2a2e6bc28bc639b3872
Michael Walker <mike@barrucadu.co.uk>
2024-11-25 21:00:34 +0000

    Migrate from docker image to nix flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      formatter.${system} = pkgs.nixpkgs-fmt;
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          python3Packages.black
+          (python3.withPackages (ps: [
+            ps.docopt
+            ps.jinja2
+            ps.markdown
+            ps.pyyaml
+          ]))
+        ];
+      };
+    };
+}
